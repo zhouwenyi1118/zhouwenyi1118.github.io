05164ee31562c85eaebc73cbb75cfed97ee2c2c3
zhouwenyi1118 <1741548662@qq,com>
2024-11-07 04:43:40 +0800

    Site updated: 2024-11-07 04:43:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/07/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2024/11/07/小周的第一篇博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
